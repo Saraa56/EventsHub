a239741250a7bee0e89ab52340e0f75f980b6d98
Saraa56 <oteronikol12@gmail.com>
2026-09-02 18:37:26 -0500

    Estructura inicial del frontend en version2

---
+++ frontend/src/components/layaout/MainLayout.tsx
@@ -0,0 +1,17 @@
+
+import { Outlet } from "react-router-dom";
+import Navbar from "../navbar/Navbar";
+
+
+
+export default function MainLayout () {
+    return (
+      <>
+            <Navbar /> 
+              <main>
+                <Outlet></Outlet>
+              </main>
+            <footer /> 
+       </> 
+    )
+}   
